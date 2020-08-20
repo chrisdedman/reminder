36a60829f174ed998282e5f5ef5d3b6da313c14f
Chris D-R <61106361+chrishaman@users.noreply.github.com>
2020-08-19 17:51:44 -0700

    Update timer_version2.py

--- timer_version2.py
+++ timer_version2.py
@@ -3,17 +3,40 @@ from AppKit import NSSound
 import time
 import os
 
-def first_time_crate():
+list = []
+# Here's your daily date append in the file: history_reminder.txt
+def todaysDate():
+    daily_clock = time.localtime()
+    daily_date = time.strftime("%a, %b %d %Y", daily_clock)
+    data = "----------------\n" + daily_date + "\n----------------"
+    with open("history_reminder.txt", 'a') as output:
+        output.write("\n" + data + '\n')
+        output.close()
+        return Alarm()
+
+def Alarm():
+    global list
     end_time1 = input("At what time do you want an alarm?\n(Format 24h: hh:mm:ss)\n>>")
     reminder = input("What's the reminder for?\n>>")
+    all = "Time: " + end_time1 + " Your Reminder is: " + reminder
+    list.append(all)
+    Last_reminder = input("Do you want to see your all reminder schedules?\n[yes/no]\n>>")
+    if 'yes' in Last_reminder:
+        print(list)
+        question = input("Are you done?")
+        if 'yes' in question:
+            pass
+    else:
+        pass
+
     its_time = False
     while its_time == False:
         its_time = False
         # ------------------------------------------ #
         # ------ Local Time in your country! ------- #
         # ------------------------------------------ #
-        named_tuple = time.localtime()  # get struct_time
-        Timer = time.strftime("%a, %b %d %Y%, %X%p (%Z)", named_tuple)
+        clock = time.localtime()  # get struct_time
+        Timer = time.strftime("%a, %b %d %Y%, %X%p (%Z)", clock)
         # ------------------------- #
         # ------ Timer Loop ------- #
         # ------------------------- #
@@ -25,17 +48,22 @@ def first_time_crate():
             # ----------------------------- #
             sound = NSSound.alloc()
             sound.initWithContentsOfFile_byReference_(
-                '/Users/chrisrollet/Desktop/Programming/Python Project/Clock Timer/Puppy Out1.m4a', True) # Add your own file where to find your song.
+                'reminder_voice.mp3', True)        # < ------------------------- Here should be your song.
             sound.play()
             # ----------------------------------------------------------------------------------------- #
-            # ------ Add 5 seconds time sleep (time the song play) before to close the program -------- #
+            # ------ Add 5 seconds time sleep (time the song play) before to close the program ------- #
             # ----------------------------------------------------------------------------------------- #
-            time.sleep(10)
+            time.sleep(8)
             while sound.isPlaying():
                 another = input("Do you want an other one?\n[yes/no]\n>>")
                 if another == 'yes':
-                    return first_time_crate()
+                    return Alarm()
                 else:
+                    # Here you print the history of your day, and append it to the history file: history_reminder.txt 
+                    print(list)
+                    with open("history_reminder.txt", 'a') as output:
+                        for row in list:
+                            output.write(str(row) + '\n')
                     break
             # --------------------------------------------- #
             # ------ Print this until the time out! ------- #
@@ -43,10 +71,11 @@ def first_time_crate():
         else:
             print("Local current time :", Timer)
             print("\tTimer end at: ", end_time1)
+            print("\tYour reminder is:", reminder)
             print("." * 50)
             time.sleep(1)
             os.system('clear')
             its_time = False
 
 if __name__ == "__main__":
-    first_time_crate()
+    todaysDate()
